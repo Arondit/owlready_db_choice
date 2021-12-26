d37c48c97e92091ca7a98327511c08aae92b6cae
Arondit <57594292+Arondit@users.noreply.github.com>
2021-12-26 18:29:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ async def root():
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Программа</title>
+            <title>Автоматический выбор базы данных</title>
             <link rel="stylesheet" href="static/style.css">
             <script src="static/script.js" defer></script>
         </head>
